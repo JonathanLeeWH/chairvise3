9fd3b4aa072f0497c1401b06763be0f221c0a6dc
JonathanLeeWH <leeweihao1996@outlook.com>
2019-09-18 22:51:08 +0800

    Update MailLogic.java with support for Mail model/entity and also better Spring Boot coding practices

--- src/main/java/sg/edu/nus/comp/cs3219/viz/logic/MailLogic.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/logic/MailLogic.java
@@ -3,6 +3,7 @@ package sg.edu.nus.comp.cs3219.viz.logic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import sg.edu.nus.comp.cs3219.viz.common.entity.Mail;
 import sg.edu.nus.comp.cs3219.viz.common.util.JavaMailUtilities;
 
 import javax.mail.Message;
@@ -21,16 +22,20 @@ public class MailLogic {
     @Value("${smtpconfiguration.mail.mailaddress}")
     private String SmtpMailAddress;
 
-    @Autowired
     private JavaMailUtilities javaMailUtilities;
 
-    public void sendMessage() {
+    @Autowired
+    public MailLogic(JavaMailUtilities javaMailUtilities) {
+        this.javaMailUtilities = javaMailUtilities;
+    }
+
+    public void sendMessage(Mail mailRequest) {
         try {
             Message message = new MimeMessage(this.javaMailUtilities.getJavaMailSession());
             message.setFrom(new InternetAddress(SmtpMailAddress));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("<Mail address to send to>"));
-            message.setSubject("Hello World");
-            message.setText("Hello World");
+            message.setRecipients(Message.RecipientType.TO, mailRequest.getMailToAsInternetAddress());
+            message.setSubject(mailRequest.getMailSubject());
+            message.setText(mailRequest.getMailContent());
             Transport.send(message);
         } catch (AddressException ex) {
             log.info("Error Sending Email: " + ex.getMessage());
