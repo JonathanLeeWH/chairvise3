e15fe7479dacab5b4eeb7f58c9df4c504ea47572
JonathanLeeWH <leeweihao1996@outlook.com>
2019-09-20 23:00:40 +0800

    Update MailController.java to support login vertification for the API request and exceptions and advices

--- src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/MailController.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/MailController.java
@@ -4,7 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sg.edu.nus.comp.cs3219.viz.common.datatransfer.UserInfo;
 import sg.edu.nus.comp.cs3219.viz.common.entity.Mail;
+import sg.edu.nus.comp.cs3219.viz.logic.GateKeeper;
 import sg.edu.nus.comp.cs3219.viz.logic.MailLogic;
 
 import java.util.logging.Logger;
@@ -13,16 +15,23 @@ import java.util.logging.Logger;
 public class MailController extends BaseRestController {
     private static final Logger log = Logger.getLogger(MailController.class.getSimpleName());
 
-    private MailLogic mailLogic;
+    private final MailLogic mailLogic;
 
-    public MailController(MailLogic mailLogic) {
+    private final GateKeeper gateKeeper;
+
+    public MailController(MailLogic mailLogic, GateKeeper gateKeeper) {
         this.mailLogic = mailLogic;
+        this.gateKeeper = gateKeeper;
     }
 
     @PostMapping("/send-mail")
     public ResponseEntity<?> sendMail(@RequestBody Mail mailRequest) {
+        UserInfo currentUser = gateKeeper.verifyLoginAccess();
+
+        log.info(currentUser.getUserEmail() + " request to send mail received.");
+
         this.mailLogic.sendMessage(mailRequest);
 
-        return ResponseEntity.accepted().build();
+        return ResponseEntity.ok().body("Mail has been sent successfully.");
     }
 }
