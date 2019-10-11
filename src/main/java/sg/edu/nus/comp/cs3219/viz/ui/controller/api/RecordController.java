52c555665f8b9cc56ebe75a72949143eb489fc53
darren96 <darren.oyk@gmail.com>
2019-10-12 01:00:04 +0800

    Update the RecordController

--- src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/RecordController.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/RecordController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sg.edu.nus.comp.cs3219.viz.common.datatransfer.UserInfo;
 import sg.edu.nus.comp.cs3219.viz.common.entity.record.AuthorRecord;
+import sg.edu.nus.comp.cs3219.viz.common.entity.record.RecordGroup;
 import sg.edu.nus.comp.cs3219.viz.common.entity.record.ReviewRecord;
 import sg.edu.nus.comp.cs3219.viz.common.entity.record.SubmissionRecord;
 import sg.edu.nus.comp.cs3219.viz.logic.GateKeeper;
@@ -27,29 +28,55 @@ public class RecordController extends BaseRestController {
         this.recordLogic = recordLogic;
     }
 
-    @PostMapping("/record/author")
-    public ResponseEntity<?> importAuthorRecord(@RequestBody List<AuthorRecord> authorRecordList) throws URISyntaxException {
+    @PostMapping("/record/record_group")
+    public ResponseEntity<?> addRecordGroup(@RequestBody String recordGroupName) throws URISyntaxException {
         UserInfo userInfo = gateKeeper.verifyLoginAccess();
+        RecordGroup recordGroup = new RecordGroup();
 
-        this.recordLogic.removeAndPersistAuthorRecordForDataSet(userInfo.getUserEmail(), authorRecordList);
+        recordGroup.setDataSet(userInfo.getUserEmail());
+        recordGroup.setRecordGroupName(recordGroupName);
+
+        this.recordLogic.addRecordGroup(recordGroup);
+
+        return ResponseEntity.created(new URI("/record/record_group")).build();
+    }
+
+    @PostMapping("/record/remove_record_group")
+    public ResponseEntity<?> removeRecordGroup(@RequestBody Long recordGroupId,
+                                               @RequestBody String recordGroupName) throws URISyntaxException {
+        gateKeeper.verifyLoginAccess();
+
+        this.recordLogic.removeAndPersistRecordGroup(recordGroupId);
+
+        return ResponseEntity.created(new URI("/record/remove_record_group")).build();
+    }
+
+    @PostMapping("/record/author")
+    public ResponseEntity<?> importAuthorRecord(@RequestBody Long recordGroupId,
+                                                @RequestBody List<AuthorRecord> authorRecordList) throws URISyntaxException {
+        gateKeeper.verifyLoginAccess();
+
+        this.recordLogic.removeAndPersistAuthorRecordForRecordGroup(recordGroupId, authorRecordList);
 
         return ResponseEntity.created(new URI("/record/author")).build();
     }
 
     @PostMapping("/record/review")
-    public ResponseEntity<?> importReviewRecord(@RequestBody List<ReviewRecord> reviewRecordList) throws URISyntaxException {
-        UserInfo userInfo = gateKeeper.verifyLoginAccess();
+    public ResponseEntity<?> importReviewRecord(@RequestBody Long recordGroupId,
+                                                @RequestBody List<ReviewRecord> reviewRecordList) throws URISyntaxException {
+        gateKeeper.verifyLoginAccess();
 
-        this.recordLogic.removeAndPersistReviewRecordForDataSet(userInfo.getUserEmail(), reviewRecordList);
+        this.recordLogic.removeAndPersistReviewRecordForRecordGroup(recordGroupId, reviewRecordList);
 
         return ResponseEntity.created(new URI("/record/review")).build();
     }
 
     @PostMapping("/record/submission")
-    public ResponseEntity<?> importSubmissionRecord(@RequestBody List<SubmissionRecord> submissionRecords) throws URISyntaxException {
-        UserInfo userInfo = gateKeeper.verifyLoginAccess();
+    public ResponseEntity<?> importSubmissionRecord(@RequestBody Long recordGroupId,
+                                                    @RequestBody List<SubmissionRecord> submissionRecords) throws URISyntaxException {
+        gateKeeper.verifyLoginAccess();
 
-        this.recordLogic.removeAndPersistSubmissionRecordForDataSet(userInfo.getUserEmail(), submissionRecords);
+        this.recordLogic.removeAndPersistSubmissionRecordForRecordGroup(recordGroupId, submissionRecords);
 
         return ResponseEntity.created(new URI("/record/review")).build();
     }
