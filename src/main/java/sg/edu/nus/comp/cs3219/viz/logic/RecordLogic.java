6ca15263d5f1f939b3a9298a8d617ffa2191b746
darren96 <darren.oyk@gmail.com>
2019-10-12 00:59:52 +0800

    Update the record logic

--- src/main/java/sg/edu/nus/comp/cs3219/viz/logic/RecordLogic.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/logic/RecordLogic.java
@@ -2,14 +2,8 @@ package sg.edu.nus.comp.cs3219.viz.logic;
 
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import sg.edu.nus.comp.cs3219.viz.common.entity.record.AuthorRecord;
-import sg.edu.nus.comp.cs3219.viz.common.entity.record.ReviewRecord;
-import sg.edu.nus.comp.cs3219.viz.common.entity.record.SubmissionAuthorRecord;
-import sg.edu.nus.comp.cs3219.viz.common.entity.record.SubmissionRecord;
-import sg.edu.nus.comp.cs3219.viz.storage.repository.AuthorRecordRepository;
-import sg.edu.nus.comp.cs3219.viz.storage.repository.ReviewRecordRepository;
-import sg.edu.nus.comp.cs3219.viz.storage.repository.SubmissionAuthorRecordRepository;
-import sg.edu.nus.comp.cs3219.viz.storage.repository.SubmissionRecordRepository;
+import sg.edu.nus.comp.cs3219.viz.common.entity.record.*;
+import sg.edu.nus.comp.cs3219.viz.storage.repository.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,57 +18,76 @@ public class RecordLogic {
 
     private ReviewRecordRepository reviewRecordRepository;
 
+    private RecordGroupRepository recordGroupRepository;
+
     public RecordLogic(AuthorRecordRepository authorRecordRepository,
                        SubmissionRecordRepository submissionRecordRepository,
                        SubmissionAuthorRecordRepository submissionAuthorRecordRepository,
-                       ReviewRecordRepository reviewRecordRepository) {
+                       ReviewRecordRepository reviewRecordRepository,
+                       RecordGroupRepository recordGroupRepository) {
         this.authorRecordRepository = authorRecordRepository;
         this.submissionRecordRepository = submissionRecordRepository;
         this.submissionAuthorRecordRepository = submissionAuthorRecordRepository;
         this.reviewRecordRepository = reviewRecordRepository;
+        this.recordGroupRepository = recordGroupRepository;
     }
 
     @Transactional
-    public void removeAndPersistAuthorRecordForDataSet(String dataSet, List<AuthorRecord> authorRecordList) {
-        authorRecordRepository.deleteAllByDataSetEquals(dataSet);
+    public void addRecordGroup(RecordGroup recordGroup) {
+        recordGroupRepository.save(recordGroup);
+    }
+
+    @Transactional
+    public void removeAndPersistRecordGroup(Long recordGroupId) {
+        recordGroupRepository.deleteById(recordGroupId);
+    }
+
+    @Transactional
+    public void removeAndPersistAuthorRecordForRecordGroup(Long recordGroupId,
+                                                       List<AuthorRecord> authorRecordList) {
+        authorRecordRepository.deleteAllByRecordGroupIdEquals(recordGroupId);
         authorRecordRepository.saveAll(authorRecordList.stream().peek(r -> {
             // should not set ID when creating records
             r.setId(null);
-            // should set dataSet
-            r.setDataSet(dataSet);
+            // set the recordGroupId
+            r.setRecordGroupId(recordGroupId);
             // the other field can be arbitrary
         }).collect(Collectors.toList()));
     }
 
     @Transactional
-    public void removeAndPersistReviewRecordForDataSet(String dataSet, List<ReviewRecord> reviewRecordList) {
-        reviewRecordRepository.deleteAllByDataSetEquals(dataSet);
+    public void removeAndPersistReviewRecordForRecordGroup(Long recordGroupId,
+                                                       List<ReviewRecord> reviewRecordList) {
+        reviewRecordRepository.deleteAllByRecordGroupIdEquals(recordGroupId);
         reviewRecordRepository.saveAll(reviewRecordList.stream().peek(r -> {
             // should not set ID when creating records
             r.setId(null);
-            // should set dataSet
-            r.setDataSet(dataSet);
+            // set the recordGroupId
+            r.setRecordGroupId(recordGroupId);
             // the other field can be arbitrary
         }).collect(Collectors.toList()));
     }
 
     @Transactional
-    public void removeAndPersistSubmissionRecordForDataSet(String dataSet, List<SubmissionRecord> submissionRecordList) {
-        submissionRecordRepository.deleteAllByDataSetEquals(dataSet);
-        submissionAuthorRecordRepository.deleteAllByDataSetEquals(dataSet);
+    public void removeAndPersistSubmissionRecordForRecordGroup(Long recordGroupId,
+                                                           List<SubmissionRecord> submissionRecordList) {
+        submissionRecordRepository.deleteAllByRecordGroupIdEquals(recordGroupId);
+        submissionAuthorRecordRepository.deleteAllByRecordGroupIdEquals(recordGroupId);
         submissionRecordRepository.saveAll(submissionRecordList.stream().peek(s -> {
             // should not set ID when creating records
             s.setId(null);
-            // should set dataSet
-            s.setDataSet(dataSet);
+            // set the recordGroupId
+            s.setRecordGroupId(recordGroupId);
             // create many to many relationship for authors
             List<SubmissionAuthorRecord> submissionAuthorRecords = s.getAuthors().stream()
                     .map(authorName -> {
-                        SubmissionAuthorRecord existing = submissionAuthorRecordRepository.findFirstByNameEqualsAndDataSetEquals(authorName, dataSet);
+                        SubmissionAuthorRecord existing =
+                                submissionAuthorRecordRepository
+                                        .findFirstByNameEqualsAndRecordGroupIdEquals(authorName, recordGroupId);
                         if (existing == null) {
                             existing = new SubmissionAuthorRecord();
-                            existing.setDataSet(dataSet);
                             existing.setName(authorName);
+                            existing.setRecordGroupId(recordGroupId);
                             existing = submissionAuthorRecordRepository.save(existing);
                         }
                         return existing;
